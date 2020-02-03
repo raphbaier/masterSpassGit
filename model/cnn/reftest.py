871ea32f80d60ec7161145059528513fd2b922c6
raphael <raphael-baier@web.de>
2020-02-03 09:19:11 +0100

    alles

--- model/cnn/reftest.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from keras.utils import np_utils, to_categorical
-
-
-ref = pd.read_csv("input/Data_path.csv")
-ref.head()
-print(ref)
--- sound_to_input/stock_info_test.py
+++
@@ -1,5 +0,0 @@
-import yfinance as yf
-
-data = yf.download('','2018-01-01','2018-01-08')
-
-strings_with_problems = ("BF.B", "BF.2", "DWDP", "HRS")
